f0ef066088010b93aeee9fe590519aaf6389dc81
chezb <chnchenzb@outlook.com>
2025-02-07 08:08:59 -0500

    finish lab5 lazy

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,25 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  }else if(r_scause() == 13 || r_scause() == 15){
+    // 获取造成页面错误的虚拟地址
+    uint64 va = r_stval();
+    char* mem = kalloc();
+    if(mem == 0){
+      p->killed = 1;
+    }else{
+      if(PGROUNDUP(p->trapframe->sp)-1 < va && va < p->sz && memset(mem, 0, PGSIZE)){
+        // 虚拟地址向下取整
+        va = PGROUNDDOWN(va);
+        if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+          kfree(mem);
+          p->killed = 1;
+        }
+      }else{
+        p->killed = 1;
+      }
+    }
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
@@ -82,6 +100,7 @@ usertrap(void)
 
   usertrapret();
 }
+
 
 //
 // return to user space
--- kernel/vm.c
+++ kernel/vm.c
@@ -181,11 +181,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue;//("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue;//panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
-      panic("uvmunmap: not a leaf");
+     panic("uvmunmap: not a leaf");
     if(do_free){
       uint64 pa = PTE2PA(*pte);
       kfree((void*)pa);
@@ -315,9 +315,9 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      continue;//("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      continue;//("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
