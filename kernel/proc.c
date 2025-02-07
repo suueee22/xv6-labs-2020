0c4186c13921abbfcfbc8fb93e8e847f2301f115
chezb <chnchenzb@outlook.com>
2025-02-07 07:44:55 -0500

    finish lab4traps

--- kernel/proc.c
+++ kernel/proc.c
@@ -113,6 +113,14 @@ found:
     return 0;
   }
 
+  // Allocate a trapframe page.
+  if((p->trapframe_copy = (struct trapframe *)kalloc()) == 0){
+    release(&p->lock);
+    return 0;
+  }
+
+  
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -126,7 +134,9 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
-
+  p->alarm_handle = 0;
+  p->alarm_interval = 0;
+  p->last_tick_time = 0;
   return p;
 }
 
@@ -139,6 +149,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->trapframe_copy)
+    kfree((void*)p->trapframe_copy);
+  p->trapframe_copy = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
--- kernel/proc.h
+++ kernel/proc.h
@@ -93,6 +93,11 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int alarm_interval;           // 报警间隔
+  void (*alarm_handle);           // 报警处理函数
+  int last_tick_time;           // 上次报警的滴答时刻
+  struct trapframe *trapframe_copy; // data page for trampoline.S copy
+  int is_alarming;      
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
